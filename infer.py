ba8be72e4b2b82229780faa504ec5366c44414e2
hieunguyen7337 <59214551+hieunguyen7337@users.noreply.github.com>
2023-04-28 10:23:37 +0700

    add commented output

--- infer.py
+++ infer.py
@@ -13,3 +13,8 @@ inputs = [
 
 outputs = model.generate(tokenizer(inputs, return_tensors="pt", padding=True).input_ids, max_length=512)
 print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+
+# ['en: VietAI is a non-profit organization with the mission of nurturing artificial intelligence talents and building an international - class community of artificial intelligence experts in Vietnam.',
+#  'en: According to the latest LinkedIn report on the 2020 list of attractive and promising jobs, AI - related job titles such as AI Specialist, ML Engineer and ML Engineer all rank high.',
+#  'vi: Nhóm chúng tôi khao khát tạo ra những khám phá có ảnh hưởng đến mọi người, và cốt lõi trong cách tiếp cận của chúng tôi là chia sẻ nghiên cứu và công cụ để thúc đẩy sự tiến bộ trong lĩnh vực này.',
+#  'vi: Chúng ta đang trên hành trình tiến bộ và dân chủ hoá trí tuệ nhân tạo thông qua mã nguồn mở và khoa học mở.']
