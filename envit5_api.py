90937ce3989bba5474dfa8a160402ddaff957e18
hieunguyen7337 <59214551+hieunguyen7337@users.noreply.github.com>
2023-05-05 14:36:49 +0700

    debug api code

--- envit5_api.py
+++ envit5_api.py
@@ -9,13 +9,27 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def whisper_predict():
-    input = request.params['text']
+    # read translation mode
+    mode = request.args.get('mode')
+
     # read input text
+    input = request.data.decode('utf-8')
+
+    if mode == "en2vi":
+        input = "en: " + input
+    else:
+        input = "vi: " + input
     inputs = [input]
 
-    outputs = model.generate(tokenizer(inputs, return_tensors="pt", padding=True).input_ids, max_length=512)
+    tokenized_inputs = tokenizer(inputs, return_tensors="pt", padding=True)
 
-    return outputs[0]
+    outputs_tokens = model.generate(tokenized_inputs.input_ids, max_length=4096)
+
+    outputs = tokenizer.batch_decode(outputs_tokens, skip_special_tokens=True)
+
+    output = outputs[0][4:]
+
+    return output
 
 if __name__ == '__main__':
     app.run(port=5000)
